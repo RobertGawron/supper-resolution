66bff596bfeea31521892e53f625e0c91a9cb20d
Robert <robert_gawron@yahoo.fr>
2010-05-03 17:52:55 +0200

    shit refactored

--- his.py
+++ his.py
@@ -3,31 +3,6 @@ import Image
 # poor code based on theory from:
 # M. Irani and S. Peleg, "Super Resolution From Image Sequences"
 
-"""for f in low_res_files:
-    low_res_images.append(Image.open(f))
-
-estimated = Image.open(files[0])
-bigger_size = (images[0].size[0]*zoom, images[0].size[1]*zoom)
-estimated = estimated.resize(bigger_size, Image.ANTIALIAS)
-
-
-for i in images:
-
-    z = i.resize(bigger_size, Image.ANTIALIAS) 
-   
-    for x in range(0, estimated.size[0]-30):
-        for y in range(0, estimated.size[1]-30):
-            (fo,_,_) = estimated.getpixel((x,y))
-            (fi,_,_) = z.getpixel((x,y)) 
-
-            v = int((fo-fi)*c)
-            estimated.putpixel((x, y), (fo+v, fo+v, fo+v))
-
-
-
-
-estimated.save(output_fname)
-"""
 
 def load_images(files):
     images = []
@@ -35,12 +10,33 @@ def load_images(files):
         images.append(Image.open(f))
     return images
 
+
 def main():
     low_res_files = ['moon1.JPG', 'moon2.JPG']
     low_res_images = load_images(low_res_files)
     output_file = 'output.JPG'
     zoom = 2
     c = 2.2
+
+    estimated = Image.open(files[0])
+    bigger_size = (images[0].size[0]*zoom, images[0].size[1]*zoom)
+    estimated = estimated.resize(bigger_size, Image.ANTIALIAS)
+    
+
+    for i in images:
+
+        z = i.resize(bigger_size, Image.ANTIALIAS) 
+   
+        for x in range(0, estimated.size[0]-30):
+            for y in range(0, estimated.size[1]-30):
+                (fo,_,_) = estimated.getpixel((x,y))
+                (fi,_,_) = z.getpixel((x,y)) 
+
+                v = int((fo-fi)*c)
+                estimated.putpixel((x, y), (fo+v, fo+v, fo+v))
+
+    estimated.save(output_fname)
+
 
 
 if __name__=="__main__":
