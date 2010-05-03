3940a8a3fc0cf0dc5458ccfd2706283afd952f7b
Robert <robert_gawron@yahoo.fr>
2010-05-03 17:50:56 +0200

    shit refactored

--- his.py
+++ his.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python
 import Image
-# this code is full of shit
+# poor code based on theory from:
+# M. Irani and S. Peleg, "Super Resolution From Image Sequences"
 
-files = ['moon1.JPG', 'moon2.JPG']
-images = []
-output_fname = 'output.JPG'
-zoom = 2
-c = 2.2
+"""for f in low_res_files:
+    low_res_images.append(Image.open(f))
 
-for f in files:
-    images.append(Image.open(f))
-
-""" create estimated image """
 estimated = Image.open(files[0])
 bigger_size = (images[0].size[0]*zoom, images[0].size[1]*zoom)
 estimated = estimated.resize(bigger_size, Image.ANTIALIAS)
 
 
 for i in images:
-    """ every image may add sth to the output """
-
 
     z = i.resize(bigger_size, Image.ANTIALIAS) 
    
@@ -32,5 +24,24 @@ for i in images:
             estimated.putpixel((x, y), (fo+v, fo+v, fo+v))
 
 
-estimated.save(output_fname)
 
+
+estimated.save(output_fname)
+"""
+
+def load_images(files):
+    images = []
+    for f in files:
+        images.append(Image.open(f))
+    return images
+
+def main():
+    low_res_files = ['moon1.JPG', 'moon2.JPG']
+    low_res_images = load_images(low_res_files)
+    output_file = 'output.JPG'
+    zoom = 2
+    c = 2.2
+
+
+if __name__=="__main__":
+    main()
