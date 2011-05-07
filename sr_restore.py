292c2271fddd472e009675e05d5c8c09d657a789
RobertGawron <robert_gawron@yahoo.fr>
2011-05-07 20:37:47 +0200

    started moving functions and data into separate classes

--- sr_restore.py
+++ sr_restore.py
@@ -41,33 +41,53 @@ class Camera:
 
 
 
-def update_estimation(high_res_image, captured_images, hps, k, s):
-    error = 0
-    mask = ((-1, -1), (0, -1), (1, -1),
-            (-1,  0), (0,  0), (1,  0),
-            (-1,  1), (0,  1), (1,  1))
+class SuperResolutionImage:
+    def __init__(self, hps):
+        self.hsp = hps
+        self.mask = ((-1, -1), (0, -1), (1, -1),
+                     (-1,  0), (0,  0), (1,  0),
+                     (-1,  1), (0,  1), (1,  1))
 
-    camera = Camera(hps)
-    for ((dx,dy), captured) in captured_images:
-        simulated = camera.take_a_photo(high_res_image, (dx, dy), s)
-        simulated = simulated.resize(high_res_image.size, Image.ANTIALIAS)
-        captured = captured.resize(high_res_image.size, Image.ANTIALIAS)
+    def restore(images, upsize):   
+    """def update_estimation(high_res_image, captured_images, hps, k, s):
+        error = 0
+        mask = ((-1, -1), (0, -1), (1, -1),
+                (-1,  0), (0,  0), (1,  0),
+                (-1,  1), (0,  1), (1,  1))
 
-        for x in range(2, simulated.size[0]-2):
-            for y in range(2, simulated.size[1]-2):
-                (rc, gc, bc) = captured.getpixel((x, y))
-                (rs, gs, bs) = simulated.getpixel((x, y))
+        camera = Camera(hps)
+        for ((dx,dy), captured) in captured_images:
+            simulated = camera.take_a_photo(high_res_image, (dx, dy), s)
+            simulated = simulated.resize(high_res_image.size, Image.ANTIALIAS)
+            captured = captured.resize(high_res_image.size, Image.ANTIALIAS)
 
-                error += abs(rc - rs) + abs(gc - gs) + abs(bc - bs)
+            for x in range(2, simulated.size[0]-2):
+                for y in range(2, simulated.size[1]-2):
+                    (rc, gc, bc) = captured.getpixel((x, y))
+                    (rs, gs, bs) = simulated.getpixel((x, y))
 
-                for (pfs_index, (dx, dy)) in zip(range(9), mask):
-                    (rh, gh, bh) = high_res_image.getpixel((x-dx-1, y-dy-1))
-                    rh += hps[pfs_index] * (rc - rs) / (k)
-                    gh += hps[pfs_index] * (gc - gs) / (k)
-                    bh += hps[pfs_index] * (bc - bs) / (k)
-                    high_res_image.putpixel((x-dx-1, y-dy-1), (rh, gh, bh))
+                    error += abs(rc - rs) + abs(gc - gs) + abs(bc - bs)
+
+                    for (pfs_index, (dx, dy)) in zip(range(9), mask):
+                        (rh, gh, bh) = high_res_image.getpixel((x-dx-1, y-dy-1))
+                        rh += hps[pfs_index] * (rc - rs) / (k)
+                        gh += hps[pfs_index] * (gc - gs) / (k)
+                        bh += hps[pfs_index] * (bc - bs) / (k)
+                        high_res_image.putpixel((x-dx-1, y-dy-1), (rh, gh, bh))
     
-    return (error, high_res_image)
+        return (error, high_res_image)
+
+    k = len(captured_images)
+
+    base_for_estimation = captured_images[0][1]
+    high_res_size = (base_for_estimation.size[0]*scale, base_for_estimation.size[1]*scale)
+    high_res_image = base_for_estimation.resize(high_res_size, Image.ANTIALIAS)
+
+    for iteration in range(0, config['iterations']):
+        (error, high_res_image) = update_estimation(high_res_image, captured_images, config['psf'], k, scale)
+        high_res_image.save('iteration.%d.tif' % iteration)
+        error /=  float(k * high_res_image.size[0] * high_res_image.size[1])
+        logging.info('iteration: %2d, estimation error: %3f' % (iteration, error))"""
 
 
 if __name__=="__main__":
@@ -76,7 +96,7 @@ if __name__=="__main__":
     default_config_file = 'config.yaml'
     default_scale_value = 2
 
-    parser = OptionParser('have a nice day')
+    parser = OptionParser()
     parser.add_option('-s', '--scale', dest='scale', 
                         help='scale factor, natural number, greater from zero')
     parser.add_option('-c', '--config', dest='config', 
@@ -96,17 +116,8 @@ if __name__=="__main__":
     config = yaml.load(config)
     logging.debug(config)
 
-    load_image = lambda (x,y): ((x,y), Image.open('%s/%d_%d.tif' % (config['samples_folder'], x, y)))
-    captured_images = map(load_image, config['offsets_of_captured_imgs'])
-    k = len(captured_images)
-
-    base_for_estimation = captured_images[0][1]
-    high_res_size = (base_for_estimation.size[0]*scale, base_for_estimation.size[1]*scale)
-    high_res_image = base_for_estimation.resize(high_res_size, Image.ANTIALIAS)
-
-    for iteration in range(0, config['iterations']):
-        (error, high_res_image) = update_estimation(high_res_image, captured_images, config['psf'], k, scale)
-        high_res_image.save('iteration.%d.tif' % iteration)
-        error /=  float(k * high_res_image.size[0] * high_res_image.size[1])
-        logging.info('iteration: %2d, estimation error: %3f' % (iteration, error))
+    sr_img = SuperResolutionImage:
+ 
+    #load_image = lambda (x,y): ((x,y), Image.open('%s/%d_%d.tif' % (config['samples_folder'], x, y)))
+    #captured_images = map(load_image, config['offsets_of_captured_imgs'])
 
