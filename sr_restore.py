4ac75d48812c65513e2973c1aef272be26563e87
RobertGawron <robert_gawron@yahoo.fr>
2011-05-08 14:25:51 +0200

    further refactoring of SR implementation

--- sr_restore.py
+++ sr_restore.py
@@ -43,13 +43,44 @@ class Camera:
 
 class SuperResolutionImage:
     def __init__(self, hps):
-        self.hsp = hps
+        self.hps = hps
         self.mask = ((-1, -1), (0, -1), (1, -1),
                      (-1,  0), (0,  0), (1,  0),
                      (-1,  1), (0,  1), (1,  1))
 
-    def restore(self, high_res, images, upsize):
+    def restore(self, camera, high_res, images, upsize):
         error = 0
+        high_res_image = high_res
+        hps = self.hps
+        mask = self.mask
+        s = upsize
+        captured_images = images
+        k = len(captured_images)
+
+       
+        for ((dx,dy), captured) in captured_images:
+            simulated = camera.take_a_photo(high_res_image, (dx, dy), s)
+            simulated = simulated.resize(high_res_image.size, Image.ANTIALIAS)
+            captured = captured.resize(high_res_image.size, Image.ANTIALIAS)
+
+            for x in range(2, simulated.size[0]-2):
+                for y in range(2, simulated.size[1]-2):
+                    (rc, gc, bc) = captured.getpixel((x, y))
+                    (rs, gs, bs) = simulated.getpixel((x, y))
+
+                    error += abs(rc - rs) + abs(gc - gs) + abs(bc - bs)
+
+                    for (pfs_index, (dx, dy)) in zip(range(9), mask):
+                        (rh, gh, bh) = high_res_image.getpixel((x-dx-1, y-dy-1))
+                        rh += hps[pfs_index] * (rc - rs) / (k)
+                        gh += hps[pfs_index] * (gc - gs) / (k)
+                        bh += hps[pfs_index] * (bc - bs) / (k)
+                        high_res_image.putpixel((x-dx-1, y-dy-1), (rh, gh, bh))
+    
+        return high_res_image, error
+
+
+
         
         return high_res, error
     """def update_estimation(high_res_image, captured_images, hps, k, s):
@@ -127,19 +158,17 @@ if __name__=="__main__":
         image = Image.open('%s/%d_%d.tif' % (config['samples_folder'], dx, dy))
         input_images.append(((dx, dy), image))
 
-    high_res_image = input_images[0][1]#.resize(
-    k = 6
+    # TODO this is broken, use some data structure or class 
+    high_res_size = input_images[0][1].size[0] * scale, input_images[0][1].size[1] * scale
+    high_res_image = input_images[0][1].resize(high_res_size, Image.ANTIALIAS)
+
+    camera = Camera(config['psf'])
+
+
     # TODO move this to separate class, that will check erorr of estimation
     for i in range(config['iterations']):
-        high_res_image, error = sr_restorator.restore(high_res_image, input_images, scale)#, k, scale)
+        high_res_image, error = sr_restorator.restore(camera, high_res_image, input_images, scale)
         high_res_image.save('iteration_%d.tif' % i)
+        error /=  float(k * high_res_image.size[0] * high_res_image.size[1])
         logging.info('iteration: %2d, estimation error: %3f' % (i, error))
-
-        #(error, high_res_image) = update_estimation(high_res_image, captured_images, config['psf'], k, scale)
-        #high_res_image.save('iteration.%d.tif' % iteration)
-        #error /=  float(k * high_res_image.size[0] * high_res_image.size[1])
-
-
-    #load_image = lambda (x,y): ((x,y), Image.open('%s/%d_%d.tif' % (config['samples_folder'], x, y)))
-    #captured_images = map(load_image, config['offsets_of_captured_imgs'])
 
