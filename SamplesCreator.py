f5778ffe70126246472c0052244cea7a0884c092
RobertGawron <robert_gawron@yahoo.fr>
2013-02-03 19:44:43 +0100

    refactoring, removed the stub () method, it's not useful at all and I don't remember why I've added it

--- SamplesCreator.py
+++ SamplesCreator.py
@@ -10,28 +10,21 @@ import logging
 import myconfig
 import Camera
 
-
-def stub(input_image):
-    logging.basicConfig(level=logging.INFO)
-    
-    config = myconfig.config
-    
-    if not os.path.exists(config['samples_folder']):
-        os.mkdir(config['samples_folder'])
-
-    camera = Camera.Camera(config['psf'])
-    downscale = 1.0/config['scale']
-
-    for (x, y) in config['offsets_of_captured_imgs']:
-        low_res_file = '%s/S_%d_%d.tif' % (config['samples_folder'], x, y)
-        camera.take_a_photo(input_image, (x, y), downscale).save(low_res_file)
-        logging.info('saved: %s' % low_res_file)
-
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     input_image = Image.open(sys.argv[1])
-    logging.info('Input Image: %s' % sys.argv[1])
-    logging.info('Image size: %dx%d' % (input_image.size[0],input_image.size[1]))
-    stub(input_image)
+    logging.info('Input image size: %dx%d' % (input_image.size[0],input_image.size[1]))
+    
+    if not os.path.exists(myconfig.config['samples_folder']):
+        os.mkdir(myconfig.config['samples_folder'])
+
+    camera = Camera.Camera(myconfig.config['psf'])
+    downscale = 1.0 / myconfig.config['scale']
+
+    for (x, y) in myconfig.config['offsets_of_captured_imgs']:
+        low_res_file = '%s/S_%d_%d.tif' % (myconfig.config['samples_folder'], x, y)
+        camera.take_a_photo(input_image, (x, y), downscale).save(low_res_file)
+        logging.info('Output image saved: %s' % low_res_file)
+
 
