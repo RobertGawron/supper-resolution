302607b8d1797312451348eca0855988bb997213
RobertGawron <robert_gawron@yahoo.fr>
2013-03-01 19:53:15 +0100

    removed pointless white spaces

--- MotionEstimator.py
+++ MotionEstimator.py
@@ -30,9 +30,8 @@ class MotionEstimator:
         x_e, y_e = 0, 0
 
         for offset in [1,2,3,4,5,6]:
-            for i in range(3+offset, width-3):
-                for j in range(3, height - 3):
-
+            for i in range(6+offset, width-6):
+                for j in range(6, height - 6):
                     if i < width-3 and i < height-3:
                         reference = 0
                         for (x, y) in self.offsets:
@@ -45,47 +44,26 @@ class MotionEstimator:
                                 x_e += local_best_fit[0]
                                 y_e += local_best_fit[1]
 
-                    else:
-                        break 
-
-        print checked, x_e*4.0/checked, y_e*4.0/checked
+        normalize = 2.1
+        print checked, x_e*1.0/checked, y_e*1.0/checked
 
         if checked == 0:
             return 0, 0
-        return (x_e*4.0)/checked, (y_e*4.0)/checked
+        return (x_e*normalize)/checked, (y_e*normalize)/checked
 
 
-
-    def _get_pixel_offset(self, i, j):
-        # todo move to a separate method
+    def _get_pixel_offset(self, x, y):
         best_fit = (0,0) 
         first_check = True
-        smallest_difference = (123, 0)
+        smallest_difference = None
 
         for (x_delta, y_delta) in self.offsets:
-            p1 = self.a.getpixel((i, j))
-            p2 = self.b.getpixel((i   + x_delta, j   + y_delta))
+            difference = 0
+            for (i, j) in self.offsets:
+                p1 = self.a.getpixel((x + i          , y + j          ))
+                p2 = self.b.getpixel((x + i + x_delta, y + j + y_delta))
+                difference += abs(p1 - p2)
 
-            p3 = self.a.getpixel((i+1, j+1))
-            p4 = self.b.getpixel((i+1 + x_delta, j+1 + y_delta))
-
-            p5 = self.a.getpixel((i-1, j-1))
-            p6 = self.b.getpixel((i-1 + x_delta, j-1 + y_delta))
-
-            p7 = self.a.getpixel((i-1, j+1))
-            p8 = self.b.getpixel((i-1 + x_delta, j+1 + y_delta))
-
-            p9 = self.a.getpixel((i+1, j-1))
-            p10 = self.b.getpixel((i+1 + x_delta, j-1 + y_delta))
-
-            p11 = self.a.getpixel((i, j-1))
-            p12 = self.b.getpixel((i + x_delta, j-1 + y_delta))
-
-            p13 = self.a.getpixel((i+1, j))
-            p14 = self.b.getpixel((i+1 + x_delta, j + y_delta))
-
-
-            difference = abs(p1 - p2) + abs(p3 - p4) + abs(p5-p6) + abs(p7-p8) + abs(p9-p10)  + abs(p11-p12) + abs(p13-p14) # TODO RGB
 
             if first_check or smallest_difference > difference:
                 first_check = False
@@ -102,19 +80,19 @@ class MotionEstimator:
                (-2, -2), (-1,  2), (0,  2), (1,  2), (2,  2))"""
 
     offsets = ((0,  0), (1,  0), (2,  0),
-                  (0,  1), (1,  1), (2,  1),
-                  (0,  2), (1,  2), (2,  2))
+               (0,  1), (1,  1), (2,  1),
+               (0,  2), (1,  2), (2,  2))
 
 
 
 if __name__=="__main__":
-
     assert(len(sys.argv) == 3)
-    files = sys.argv[1], sys.argv[2]
 
+    files = sys.argv[1], sys.argv[2]
     images = map(Image.open, files)
 
     estimator = MotionEstimator(images[0], images[1])
     (x, y) = estimator.offset()
+
     print "the offset between %s and %s is (%2d, %2d)" % (files[0], files[1], x, y)
 
